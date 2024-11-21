346e7ad32dd33462f3f3baef83f08388cccbf3c9
Maxim Kuzmin <maxim.kuzmin@inbox.ru>
2024-11-22 02:23:00 +0300

    -

--- tests/Infrastructure.Tests/GlobalUsings.cs
+++ tests/Infrastructure.Tests/GlobalUsings.cs
@@ -1,4 +1,6 @@
 ﻿global using EasyNetQ;
 global using Fibonacci.DomainModel.Calculation;
+global using Fibonacci.DomainUseCases.Calculation;
+global using Fibonacci.Infrastructure.Calculation.Local.Subscriber;
 global using Fibonacci.Infrastructure.Calculation.Subscriber;
 global using Moq;
